e5b4176e0a586d5506400510e2970ae8133e4d81
patrickjaguski <patrick_jaguski@hotmail.com>
2023-10-31 22:43:43 -0300

    ajjustando git users

--- next.config.js
+++ next.config.js
@@ -1,15 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
-
-module.exports = {
-    images: {
-        remotePatterns: [
+ 
+module.exports =
+{
+    images : {
+        remotePatterns:[
             {
-                protocol:'https',
-                hostname:"httpstatusdogs.com",
+                protocol: "https",
+                hostname: "httpstatusdogs.com",
                 port: "",
-                pathname: "/img/**",
+                pathname: "/img/**"
+            },
+            {
+                protocol: "https",
+                hostname: "www.paodeacucar.com",
+                port: "",
+                pathname: "/img/**"
+            },
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+                port: "",
+                pathname: "/u/**"
             }
-        ]
-    }
+        ],
+    },
 }
