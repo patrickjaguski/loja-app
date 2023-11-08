2bde6bb34d76c024611dedfb0b434a9f5dd3ec91
patrickjaguski <patrick_jaguski@hotmail.com>
2023-11-07 21:55:10 -0300

    corrigindo login com o post

--- src/app/api/base/base-users/route.jsx
+++ src/app/api/base/base-users/route.jsx
@@ -1,8 +1,6 @@
 import {promises as fs} from 'fs';
 import { NextResponse } from 'next/server';
 
-
-
 export async function GET(request, {params}) {
     
     const file  = await  fs.readFile(process.cwd() + '/src/app/api/base/data.json', 'utf8');
@@ -18,3 +16,23 @@ export async function GET(request, {params}) {
     }
 }
 
+export async function POST(request, response){
+    const file  = await  fs.readFile(process.cwd() + '/src/app/api/base/data.json', 'utf8');
+    
+    const usuarios = await JSON.parse(file);
+
+    const userRequest = await request.json();
+
+    try{
+        for (let x = 0; x < usuarios.usuarios.length; x++) {
+            const userFile = usuarios.usuarios[x];
+
+            if(userFile.email == userRequest.email && userFile.senha == userRequest.senha){
+                return NextResponse.json({"status":true});
+            }
+        }
+    }catch(error){
+        console.log(error);
+    }
+    return NextResponse.json({"status":false});
+}
